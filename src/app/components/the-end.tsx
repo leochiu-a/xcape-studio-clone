4b7b5493862e1ce0a0d0add094cd338ac791da93
leochiu <leochiu.life@gmail.com>
2024-12-05 02:28:22 +0800

    feat: add the end section

--- src/app/components/the-end.tsx
+++ src/app/components/the-end.tsx
@@ -1,5 +1,21 @@
+import { motion } from "motion/react";
+
 const TheEnd = () => {
-  return <div className="h-screen bg-[#F1F1F1]">TheEnd</div>;
+  return (
+    <div className="bg-[#F1F1F1] py-20">
+      <motion.div
+        className="rounded-3xl w-[90%] uppercase bg-gray-950 text-8xl p-20 mx-auto text-center font-semibold"
+        whileHover={{ scale: 1.05 }}
+        transition={{ type: "spring", stiffness: 400, damping: 10 }}
+      >
+        Inspired from XSCAPE.STUDIO
+      </motion.div>
+
+      <div className="uppercase text-8xl mt-20 mx-auto text-center text-gray-950 font-semibold">
+        LEO.CHIU.WEB
+      </div>
+    </div>
+  );
 };
 
 export default TheEnd;
