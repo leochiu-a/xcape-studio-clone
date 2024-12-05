f187084e66bbd6e4fcc08552e998687527811d12
leochiu <leochiu.life@gmail.com>
2024-12-05 10:42:08 +0800

    feat: change font to roboto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  weight: ["400", "700"],
+  subsets: ["cyrillic"],
+});
 
 export const metadata: Metadata = {
   title: "XSCAPE STUDIO CLONE",
@@ -13,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={roboto.className}>{children}</body>
     </html>
   );
 }
