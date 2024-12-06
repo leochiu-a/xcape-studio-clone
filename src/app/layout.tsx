464eb4b6ff154fec026c1ff8f0762d1ed6d3d897
leochiu <leochiu.life@gmail.com>
2024-12-06 13:11:13 +0800

    feat: add next/analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
-import "./globals.css";
 import { Roboto } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+
+import "./globals.css";
 
 const roboto = Roboto({
   weight: ["400", "700"],
@@ -19,6 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Analytics />
       <body className={roboto.className}>{children}</body>
     </html>
   );
